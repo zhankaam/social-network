0c72c83177f088e0cbc8f1cbdef7e4a088a6bbf6
zhankaam <zhankaam@gmail.com>
2021-01-25 12:57:14 +0300

    social-network

--- src/Profile/MyPosts/ProfileInfo/ProfileInfo.tsx
+++ src/Profile/MyPosts/ProfileInfo/ProfileInfo.tsx
@@ -14,7 +14,8 @@ const ProfileInfo = (props: ProfileInfoPropsType) => {
     return (
         <div>
         <div className={s.img}>
-        <img src='https://sun9-36.userapi.com/impg/HZnWrqzamma2_Yw7xcSD-8XXR_IZKSxjJp0WNQ/EYak3s-RlnY.jpg?size=1080x1079&quality=96&proxy=1&sign=8743d906f74b4baec37a9fb4f8481d8a&type=album' />
+        <img src='https://sun9-36.userapi.com/impg/HZnWrqzamma2_Yw7xcSD-8XXR_IZKSxjJp0WNQ/EYak3s-RlnY
+.jpg?size=1080x1079&quality=96&proxy=1&sign=8743d906f74b4baec37a9fb4f8481d8a&type=album' />
         </div>
         <div className={s.descriptionBlock}>
             <img src={props.profile.photos.large}/>
