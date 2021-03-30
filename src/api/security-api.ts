9ce3bff65718991f112c3e8771b0e481f562958d
zhankaam <zhankaam@gmail.com>
2021-03-30 19:54:24 +0300

    social-network

--- src/api/security-api.ts
+++ src/api/security-api.ts
@@ -2,6 +2,10 @@ import {instance} from "./api";
 
 export const securityAPI = {
     getCaptchaUrl() {
-        return instance.get(`security/get-captcha-url`)
+        return instance.get<GetCaptchaUrlResponseType>(`security/get-captcha-url`)
     }
+}
+
+type GetCaptchaUrlResponseType = {
+    url:string
 }
