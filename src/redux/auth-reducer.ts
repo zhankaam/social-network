8a4c5e97abc7b3f69206bf42ddb047ad0561e62a
zhankaam <zhankaam@gmail.com>
2021-03-30 23:12:08 +0300

    social-network

--- src/redux/auth-reducer.ts
+++ src/redux/auth-reducer.ts
@@ -4,10 +4,6 @@ import {BaseThunkType, InferActionsType} from "./redux-store";
 import {authAPI} from "../api/auth-api";
 import {securityAPI} from "../api/security-api";
 
-const SET_USER_DATA = 'samurai-network/auth/SET_USER_DATA'
-const GET_CAPTCHA_URL_SUCCESS = 'samurai-network/auth/GET_CAPTCHA_URL_SUCCESS'
-
-
 let initialState = {
     userId: null as number | null,
     email: null as string | null,
@@ -18,8 +14,8 @@ let initialState = {
 
 const authReducer = (state = initialState, action: ActionsType): InitialStateType => {
     switch (action.type) {
-        case SET_USER_DATA:
-        case GET_CAPTCHA_URL_SUCCESS:
+        case 'SN/auth/SET_USER_DATA':
+        case 'SN/auth/GET_CAPTCHA_URL_SUCCESS':
             return {
                 ...state,
                 ...action.payload
@@ -31,11 +27,10 @@ const authReducer = (state = initialState, action: ActionsType): InitialStateTyp
 
 
 export const actions = {
-    setAuthUserData: (userId: number | null,
-                      email: string | null,
-                      login: string | null,
-                      isAuth: boolean) => ({type: SET_USER_DATA, payload:{userId, email,login, isAuth} } as const),
-    getCaptchaUrlSuccess: (captchaUrl: string ) => ({type: GET_CAPTCHA_URL_SUCCESS, payload:{captchaUrl} } as const),
+    setAuthUserData: (userId: number | null, email: string | null, login: string | null, isAuth: boolean) => (
+        {type: 'SN/auth/SET_USER_DATA', payload:{userId, email,login, isAuth} } as const),
+    getCaptchaUrlSuccess: (captchaUrl: string ) => (
+        {type: 'SN/auth/GET_CAPTCHA_URL_SUCCESS', payload:{captchaUrl} } as const),
 
 }
 
@@ -49,7 +44,7 @@ export const actions = {
             }
     }
 
-    export const login = (email:string, password:string,rememberMe:boolean,captcha: string) => async (dispatch: any) => {
+    export const login = (email:string, password:string,rememberMe:boolean,captcha: string): ThunkType => async (dispatch) => {
          let data = await authAPI.login(email,password,rememberMe,captcha);
          if(data.resultCode === ResultCodeEnum.Success){
              dispatch(getAuthUserData())
@@ -80,5 +75,5 @@ export const actions = {
 
 export type InitialStateType = typeof initialState;
 type ActionsType = InferActionsType<typeof actions>
-type ThunkType = BaseThunkType<ActionsType>
+type ThunkType = BaseThunkType<ActionsType | ReturnType<typeof stopSubmit>>
 export default authReducer;
