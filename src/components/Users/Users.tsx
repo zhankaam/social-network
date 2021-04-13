6d48f848e6767c50467d8eab297e3ebd3a4b1831
zhankaam <zhankaam@gmail.com>
2021-04-13 21:57:36 +0300

    typeScript for all!

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import {UserType} from "../../types";
 import {Paginator} from "../../assets/common/Paginator/Paginator";
 import {User} from "./User";
+import { Formik } from "formik/dist/Formik";
+import {ErrorMessage, Form} from "formik";
+import {Field} from "redux-form";
 
 type PropsType = {
     onPageChanged: (p: number) => void
@@ -35,5 +38,41 @@ export let Users = (props: PropsType ) => {
                                            followingInProgress={props.followingInProgress}/>)}
 
             </div>
+    </div>
+}
+
+const usersSearchFormValidate = (values: UsersSearchFormObjectType) => {
+     const errors = {}
+     return errors
+}
+type UsersSearchFormObjectType = {
+    term: string
+}
+
+const UsersSearchForm = () => {
+
+    const submit = (values: UsersSearchFormObjectType,{ setSubmitting}: {setSubmitting: (setSubmitting: boolean) => void}) => {
+          setTimeout(() => {
+              alert(JSON.stringify(values,null,2))
+              setSubmitting(false)
+          },400)
+    }
+
+    return <div>
+        <Formik
+            initialValues={{ term: ''}}
+            validate={usersSearchFormValidate}
+            onSubmit={submit}
+        >
+            {({ isSubmitting }) => (
+                <Form>
+                    <Field type="text" name="term" />
+                    <button type="submit" disabled={isSubmitting}>
+                        Find
+                    </button>
+                </Form>
+            )}
+        </Formik>
+
     </div>
 }
